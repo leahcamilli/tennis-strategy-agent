c07b16d7775c90f4cf15022f7c4ba41f7605dfde
Leah Camilli <leahcamilli@Leahs-MacBook-Pro.local>
2026-05-02 13:36:39 -0500

    Initial iOS app setup

---
+++ TennisStrategyAgent/TennisStrategyAgent/TennisStrategyAgentApp.swift
@@ -0,0 +1,32 @@
+//
+//  TennisStrategyAgentApp.swift
+//  TennisStrategyAgent
+//
+//  Created by Leah Camilli on 5/2/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TennisStrategyAgentApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ TennisStrategyAgent/TennisStrategyAgentTests/TennisStrategyAgentTests.swift
@@ -0,0 +1,18 @@
+//
+//  TennisStrategyAgentTests.swift
+//  TennisStrategyAgentTests
+//
+//  Created by Leah Camilli on 5/2/26.
+//
+
+import Testing
+
+struct TennisStrategyAgentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
